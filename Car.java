6b84a5e46411433342483f9e81cf677d144feb10
hellohi <105104807+gannyexists@users.noreply.github.com>
2025-06-09 22:42:20 +0700

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,10 +1,102 @@
 package group3_assignment;
 
+import java.io.Serializable;
+
+/**
+ * Date: /2025
+ * @author Phạm Thành Nam - HE204565
+ * Description: Car class representing details of a vehicle.
+ * Version: 1
+ */
+
+public class Car implements Serializable {
+    // Attributes
+    private String carID;
+    private Brand brand;
+    private String color;
+    private String frameID;
+    private String engineID;
+
+    // Constructors
+    public Car() {
+    }
+
+    public Car(String carID, Brand brand, String color, String frameID, String engineID) {
+        this.carID = carID;
+        this.brand = brand;
+        this.color = color;
+        this.frameID = frameID;
+        this.engineID = engineID;
+    }
+
+    // Getters
+    public String getCarID() {
+        return carID;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getFrameID() {
+        return frameID;
+    }
+
+    public String getEngineID() {
+        return engineID;
+    }
+
+    // Setters
+    public void setCarID(String carID) {
+        this.carID = carID;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFrameID(String frameID) {
+        this.frameID = frameID;
+    }
+
+    public void setEngineID(String engineID) {
+        this.engineID = engineID;
+    }
+
+    // Methods
+    @Override
+    public String toString() {
+        return "<" + carID + ", " + brand + ", " + color + ", " + frameID + ", " + engineID + ">";
+    }
+
+    public void screenString() {
+        System.out.println("<" + brand + ">\n" + carID + ", " + color + ", " + frameID + ", " + engineID + ">");
+    }
+
+    public int compareTo(Car car) {
+        int temp = brand.getName().compareTo(car.brand.getName());
+        return (temp != 0) ? temp : carID.compareTo(car.carID);
+    }
+}
+
+
+
+/*
+package group3_assignment;
+
 /**
  *
  * @author Phạm Thành Nam - HE204565
  */
-
+/*
 import java.io.Serializable; 
 
 public class Car implements Serializable {
@@ -90,4 +182,4 @@ public class Car implements Serializable {
         int temp = brand.getName().compareTo(car.brand.getName());      
         return (temp!=0)? temp : carID.compareTo(car.carID);
     }
-}
+}*/
